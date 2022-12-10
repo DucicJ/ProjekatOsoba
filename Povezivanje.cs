945644d3bec1bda5ab281d7b09a7d4af5264174c
PC <PC@DESKTOP-TB5074K>
2022-12-10 23:55:40 +0100

    Add project files.

---
+++ Povezivanje.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ProjekatOsoba
+{
+    class Povezivanje
+    {
+        public static SqlConnection Konekcija()
+        {
+            SqlConnection veza = new SqlConnection("Data Source=DESKTOP-TB5074K\\SQLEXPRESS;Initial Catalog=ednevnik2022;Integrated Security=true");
+            return veza;
+        }
+    }
+}
